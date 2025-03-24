62482daf9f798ffb834e2113bddad6e66f025ea4
Hawraa <hawraalasad@yahoo.com>
2025-03-25 00:38:49 +0300

    Day1

---
+++ Day1.ts
@@ -0,0 +1,19 @@
+console.log("Hello,World!");
+
+let variableName = "value";
+
+let username = "Amjad";
+const DOB = 2000;
+
+username = "Aziz";
+
+let wheels = 2.5;
+wheels = 4;
+
+if (wheels == 4) console.log("pass");
+else console.log("no pass");
+
+let pass = false;
+
+if (pass) console.log("sell the car");
+else console.log("dont sell");
